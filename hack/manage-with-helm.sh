e3b17113d1ee50c3a9de2a2d1fb5fc05aab4a9e5
Neil French <neilmfrench2@gmail.com>
2023-02-17 15:56:05 -0800

    Fix pvc

--- hack/manage-with-helm.sh
+++ hack/manage-with-helm.sh
@@ -3,6 +3,6 @@ NAME=$2
 RELEASE_NAME=$3
 NAMESPACE=$4
 
-kubectl annotate $KIND $NAME meta.helm.sh/release-name=$RELEASE_NAME
-kubectl annotate $KIND $NAME meta.helm.sh/release-namespace=$NAMESPACE
-kubectl label $KIND $NAME app.kubernetes.io/managed-by=Helm
+kubectl -n $NAMESPACE annotate $KIND $NAME meta.helm.sh/release-name=$RELEASE_NAME
+kubectl -n $NAMESPACE annotate $KIND $NAME meta.helm.sh/release-namespace=$NAMESPACE
+kubectl -n $NAMESPACE label $KIND $NAME app.kubernetes.io/managed-by=Helm
